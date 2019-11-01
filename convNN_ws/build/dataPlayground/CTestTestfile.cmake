0d5859e4216583935854990f41356d1b2f9db78b
fizzer <fizzer@enph353.JoDo>
2019-11-01 16:11:32 -0700

    added the world for the convNN_ws so that we can take images in it

---
+++ convNN_ws/build/dataPlayground/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/fizzer/enph353-team-grimm/convNN_ws/src/dataPlayground
+# Build directory: /home/fizzer/enph353-team-grimm/convNN_ws/build/dataPlayground
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
