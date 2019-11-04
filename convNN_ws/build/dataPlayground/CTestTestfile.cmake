fe932670ec0dff947de59c4017f7cf8be30e7019
fizzer <fizzer@enph353.JoDo>
2019-11-03 16:13:20 -0800

    fixed the image saver python script and fixed the convolution NN workspace, deleted old one and added new one

--- convNN_ws/build/dataPlayground/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fizzer/enph353-team-grimm/convNN_ws/src/dataPlayground
-# Build directory: /home/fizzer/enph353-team-grimm/convNN_ws/build/dataPlayground
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
---
+++ convolutionNN_ws/src/2019F_competition_student-master/enph353/enph353_gazebo/myScripts/imageSaver-grimmCOPY.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+# This script saves the images from the video feed of the robot whilte the
+# robot is being driven by the user (user controlled)
+
+
+from __future__ import print_function
+
+import roslib
+roslib.load_manifest('enph353_gazebo')
+import sys
+import rospy
+import cv2
+import time
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+from geometry_msgs.msg import Twist
+
+
+class robotPhotographer:
+
+    def __init__(self):
+        print("Node initialized")
+        self.bridge = CvBridge()
+        self.fileNameIncrement = 0
+        self.npcType = "potato"  # change based on which one is wanted
+
+        # Need to initial subscriber for subscribing to image feed
+        self.imageSubscriber = rospy.Subscriber("/R1/pi_camera/image_raw", Image, self.callback)
+
+    def callback(self, data):
+        # sleeps for 5 seconds
+        # we don't need every frame saved, just the significant ones
+        print("Callback started")
+        # rospy.sleep(5)
+
+        try:
+            robotImage = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+        # create filename for image
+        filename = self.fileNameCreator(self.npcType)
+
+        # save image using photographer
+        self.photographer(robotImage, filename)
+
+    """
+    @brief: Saves current frame of robot camera feed to the images
+            folder in the same directory as this script
+    @param: robotImage: cv image from robot camera plugin
+            filename: string containing filename of image without .jpg
+    """
+    def photographer(self, robotImage, filename):
+        # saves image to a file in current directory
+
+        filenameWithExtension = "grimmNPC_images/" + filename + ".jpg"
+        print("Filename with extension: ", filenameWithExtension)
+
+        cv2.imwrite("grimmNPC_images/" + filename + ".jpg", robotImage)
+
+        print("Image saved")
+
+    """
+    @brief: Creates a filename for image
+    @param: npc: raptor or pedistrian, specified at top of script
+    @return: string containing filename without .jpg
+    """
+    def fileNameCreator(self, npc):
+        self.fileNameIncrement = self.fileNameIncrement + 1
+        filename = npc + "_" + str(self.fileNameIncrement)
+        print("Filename: " + filename)
+        return filename
+
+
+"""
+@brief: Main function that calls on the robotPhotographer class
+        and makes an instance of it
+
+"""
+def main(args):
+    rb = robotPhotographer()
+    rospy.init_node('robotPhotographer', anonymous=True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main(sys.argv)
